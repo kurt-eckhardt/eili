aa616712f2a8f116463cd7e3cd464c14b8b18fbd
keckhardt <kurt.eckhardt@oncue.com>
2019-10-07 15:49:07 -0700

    Adding comments and Leet0003

--- leet/src/main/java/Leet0001.java
+++ leet/src/main/java/Leet0001.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 
 /**
- * Two Sum
+ * Problem #:  0001
+ * Name:       Two Sum
+ * Tags:       HashMap
+ * Difficulty: Easy
  *
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -13,10 +16,10 @@ import java.util.HashMap;
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  *
- * // Approach #1: HashSet
+ * Approach #1: HashMap
  * [-3, 2, 3, 4, 5, 10, 13, 14, -8, 20, 21, 22]  target=14
  *
- * // Approach #2: walk two pointers inward until they sum to target (works for sorted list only)
+ * Approach #2: walk two pointers inward until they sum to target (works for sorted list only)
  * [-3, 2, 3, 4, 5, 10, 13, 14, 16, 20, 21, 22]  target=-3 // -3+needed=-3 needed=target-num
  * needed = 5-(-3) = 8
  */
---
+++ leet/src/main/java/Leet0003.java
@@ -0,0 +1,52 @@
+public class Leet0003 {
+
+    // curIdx  aChar  lastSeenAt  startIdx   curLen   maxLen
+    // 0       a      a0          0          1        1
+    // 1       b      a0,b1       0          2        2
+    // 2       c      a0,b1,c2    0          3        3
+    // 3       a      a3,b1,c2    1          3        3
+    public static void main(String[] args) {
+        System.out.println(Leet0003.solution("This abxaaci is a string with ! and punctuations."));
+    }
+
+    public static int solution(String s) {
+
+        Integer[] lastSeenAt = new Integer[26]; // index where chars was last seen
+        int startIdx = 0; // index where the substring starts
+        int curLen   = 0;
+        int maxLen   = 0;
+        char[] chars = s.toCharArray();
+
+        for (int curIdx=0; curIdx < chars.length; curIdx++) {
+            char aChar = chars[curIdx];
+            if (!isCharInPattern(aChar, startIdx, lastSeenAt)) {
+                curLen++;
+                maxLen = Math.max(maxLen, curLen);
+            }
+            // move startIdx 1 position past the last place we saw this character
+            else {
+                int lastSeenIdx = lastSeenAt[indexFor(aChar)];
+                startIdx = lastSeenIdx+1;
+                curLen   = curIdx - startIdx + 1;
+            }
+            lastSeenAt[indexFor(aChar)] = curIdx;
+        }
+
+        return maxLen;
+    }
+
+
+    public static boolean isCharInPattern(char aChar, int startIdx, Integer[] lastSeenAt) {
+        Integer lastSeenIdx = lastSeenAt[indexFor(aChar)];
+        if (lastSeenIdx != null && lastSeenIdx >= startIdx) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static int indexFor(char aChar) {
+        return aChar-' ';
+    }
+}
